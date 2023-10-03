81a28ce2a940d4f0e097934156542141ac5b80b4
Patrick Daniel <contato.pdani@gmail.com>
2023-10-03 11:57:40 -0300

    refractor: Change in order of functions

--- assets/script/game.js
+++ assets/script/game.js
@@ -21,6 +21,48 @@ let game = {
         "react",
     ],
 
+    //cria o id aleatório pras cartas
+    create_id_with_tech: (tech) => {
+        return tech + parseInt(Math.random() * 100);
+    },
+
+    // cria os pares de cartas
+    create_pair_from_techs: (tech) => {
+        return [{
+            id: game.create_id_with_tech(tech),
+            icon: tech,
+            flipped: false,
+        },
+        {
+            id: game.create_id_with_tech(tech),
+            icon: tech,
+            flipped: false,
+        }]
+    },
+
+    //cria as cartas do tabuleiro
+    create_cards_from_techs: () => {
+        game.cards = [];
+        for (const tech of game.techs) {
+            game.cards.push(game.create_pair_from_techs(tech));
+        }
+        game.cards = game.cards.flatMap(pair => pair);
+        game.shuffle_cards();
+    },
+
+    // embaralha as cartas
+    shuffle_cards: () => {
+        let current_index = game.cards.length;
+        let random_index = 0;
+
+        while (current_index !== 0) {
+            random_index = Math.floor(Math.random() * current_index);
+            current_index--;
+            [game.cards[random_index], game.cards[current_index]] = [game.cards[current_index], game.cards[random_index]];
+        }
+    },
+
+
     set_card: (id) => {
         // coloca as cartas clicadas na memoria
         let card = game.cards.filter(card => { return card.id === id })[0];
@@ -66,49 +108,8 @@ let game = {
         game.lock_mode = false;
     },
 
-    // checa se o jogo acabou
-    check_game_over: () => {
+    check_game_over() {
+        // checa se o jogo acabou
         return game.cards.filter((cards) => { return !cards.flipped }).length === 0;
-    },
-
-    //cria as cartas do tabuleiro
-    create_cards_from_techs: () => {
-        game.cards = [];
-        for (const tech of game.techs) {
-            game.cards.push(game.create_pair_from_techs(tech));
-        }
-        game.cards = game.cards.flatMap(pair => pair);
-        game.shuffle_cards();
-    },
-
-    // cria os pares de cartas
-    create_pair_from_techs: (tech) => {
-        return [{
-            id: game.create_id_with_tech(tech),
-            icon: tech,
-            flipped: false,
-        },
-        {
-            id: game.create_id_with_tech(tech),
-            icon: tech,
-            flipped: false,
-        }]
-    },
-
-    //cria o id aleatório pras cartas
-    create_id_with_tech: (tech) => {
-        return tech + parseInt(Math.random() * 100);
-    },
-
-    // embaralha as cartas
-    shuffle_cards: () => {
-        let current_index = game.cards.length;
-        let random_index = 0;
-
-        while (current_index !== 0) {
-            random_index = Math.floor(Math.random() * current_index);
-            current_index--;
-            [game.cards[random_index], game.cards[current_index]] = [game.cards[current_index], game.cards[random_index]];
-        }
     },
 }
